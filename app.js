e45efde4f2eaa6f11b4d1a7ae8dde715ccf76709
Christina Giaramani <76108753+ChristinaGiar@users.noreply.github.com>
2021-03-27 00:02:38 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -29,3 +29,38 @@ for (link of links) {
     })
 }
 
+
+// scroll - change style of logo and menu button ---------------------
+
+let body = document.querySelector("body");
+let img = document.querySelector("img");
+
+let navImg = document.querySelector(".navbar img");
+let intro = document.querySelector('#intro');
+let blackBg = document.querySelector(".black-bg");
+let partners = document.querySelector('#partners');
+let footer = document.querySelector("footer");
+
+let navButton = document.querySelector(".navbar-toggler");
+
+document.addEventListener('scroll', function () {
+    if (window.pageYOffset >= partners.scrollHeight + intro.scrollHeight + blackBg.scrollHeight) {
+        navImg.classList.add('brand-border')
+        navButton.classList.add('nav-button-black')
+
+    }
+    else if (window.pageYOffset >= intro.scrollHeight + blackBg.scrollHeight) {
+        navImg.classList.remove('brand-border')
+        // navButton.style.backgroundColor = "none";
+        navButton.classList.remove('nav-button-black')
+
+    }
+    else if (window.pageYOffset >= intro.scrollHeight) {
+        navImg.classList.add('brand-border')
+        // navButton.style.backgroundColor = "black";  it only change once
+        navButton.classList.add('nav-button-black')
+
+    }
+
+})
+
